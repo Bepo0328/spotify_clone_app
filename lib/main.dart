96cae30961885877bc82f6c0a0c0125ee665fcb4
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-04-01 20:59:24 +0900

    SpotifyCloneApp: Create Side Menu

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:spotify_clone_app/widgets/widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -70,12 +71,8 @@ class Shell extends StatelessWidget {
         children: [
           Expanded(
             child: Row(
-              children: [
-                Container(
-                  height: double.infinity,
-                  width: 280,
-                  color: Colors.green,
-                ),
+              children: const [
+                SideMenu(),
                 // PlaylistScreen
               ],
             ),
