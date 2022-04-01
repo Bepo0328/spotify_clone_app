8e2434fa3cc9a3fb34f0c92d85bc9a89dc150955
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-04-01 20:02:15 +0900

    SpotifyCloneApp: Project Setting

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Spotify Clone App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Main Screen'),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        backgroundColor: const Color(0xFF121212),
+        primaryColor: Colors.black,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: const Color(0xFF1DB954),
+        ),
+        iconTheme: const IconThemeData().copyWith(color: Colors.white),
+        fontFamily: 'Montserrat',
+        textTheme: TextTheme(
+          headline2: const TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            fontSize: 12,
+            color: Colors.grey[300],
+            fontWeight: FontWeight.w500,
+            letterSpacing: 2,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.grey[300],
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.grey[300],
+            letterSpacing: 1,
+          ),
         ),
       ),
+      home: const Scaffold(),
     );
   }
 }
